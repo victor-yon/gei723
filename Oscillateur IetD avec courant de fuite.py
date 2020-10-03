08ac5e46120c7dabfc7d55f164fc58a2556e37ba
TonyBoy22 <31598298+TonyBoy22@users.noreply.github.com>
2020-10-03 16:38:03 -0400

    Changements sur l'oscillateur

--- Oscillateur IetD avec courant de fuite.py
+++ Oscillateur IetD avec courant de fuite.py
@@ -2,6 +2,7 @@
 
 ## Import
 from brian2 import *
+import matplotlib as plt
 
 ## [Neurone sans fuite]
 #on définit 3 vitesses possible, enclenché par la touche a "avancer"
@@ -28,26 +29,28 @@ th : 1
 vitesse : 1
 
 '''
-vitesse = 1
+vitesse = 4
 
 
 G = NeuronGroup(2, eqs, threshold= 'v >= th', reset= 'v = 0', method='exact')
 G.th = [0.8,0.8]
 G.I = [2, 0]
-G.tau = [10/vitesse, 50/vitesse]*ms
-td = -G.tau*log(1-G.th/G.I)
-w = G.th/(1+exp(-td/(50*second))+exp(-2*td/(50*second))+exp(-3*td/(50*second)))
+G.tau = [10/vitesse, 10/vitesse]*ms
+td = G.tau*((1-G.th/2)+1/2*((1-G.th/2)-1)**2)#/G.I)
+w = G.th/(1+exp(-td/(G.tau[1]))+exp(-2*td/(G.tau[1]))+exp(-3*td/(G.tau[1])))
 poid0 = w[0]
+# print(w)
+# print(1+exp(-td/(G.tau[1]))+exp(-2*td/(G.tau[1]))+exp(-3*td/(G.tau[1])))
+# print(td)
 
-
-S1 = Synapses(G, G, on_pre='v_post += poid0 ', on_post = '''I_pre = 0; I_post = 2; tau_pre = int(50/vitesse)*second ; tau_post = int(10/vitesse)*second''')
+S1 = Synapses(G, G, on_pre='v_post += 2*poid0; I_pre = 0; I_post = 2; tau_pre = (10/vitesse)*second ; tau_post = (10/vitesse)*second')
 S1.connect(i=0, j=1)
 S1.connect(i = 1, j=0)
 
 #S1.w = 'th/(1+exp(-td/(50*second))+exp(-2*td/(50*second))+exp(-3*td/(50*second)))'
 #attention, ici je n'ai pas mis tau_post mais j'ai rentré 50/vitesse
 
-S1.delay = '0*ms'
+S1.delay = 10*ms
 
 
 
@@ -61,4 +64,4 @@ plot(M.t/ms, M.v[1], label='Neuron 1')
 xlabel('Time (ms)')
 ylabel('v')
 legend()
-plt.show()
+show()
