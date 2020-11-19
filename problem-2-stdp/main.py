833d8c8d15272ecce413920942cee26ace5c4979
Victor Yon <yonvictorfr@gmail.com>
2020-11-19 10:19:40 -0500

    Final main

--- problem-2-stdp/main.py
+++ problem-2-stdp/main.py
@@ -11,11 +11,10 @@ if __name__ == '__main__':
 
     # Set simulation parameters
     # See file "simulation_parameters" for all possible parameters and default values
-    parameters = SimulationParameters(run_name='run-13',
-                                      nb_train_samples=400,
-                                      nb_test_samples=100,
-                                      nu_pre=0.005,
-                                      nu_post=0.1,
+    parameters = SimulationParameters(run_name='tmp',
+                                      classification_type='group',
+                                      nb_train_samples=2500,
+                                      nb_test_samples=500,
                                       normalization=False)
 
     # Run the beast
